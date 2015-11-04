591f76a3f2347827fec2184e78d06685bc4c7732
Robie Noor <robienoor@hotmail.co.uk>
2015-11-04 17:04:10 +0000

    Attempting to pull out the messages from fourm html

--- forumScraper.py
+++ forumScraper.py
@@ -3,13 +3,12 @@ import csv
 import requests
 
 # Insert the forum link here. Gather the HTML from the forum into a requests oject. Insert
-results = requests.get('https://forum.breastcancercare.org.uk/t5/Chemotherapy/Not-sure-if-to-have-chemo-or-not/td-p/723779')
-forumContent = results.content
+forumURL = "https://forum.breastcancercare.org.uk/t5/Chemotherapy/Not-sure-if-to-have-chemo-or-not/td-p/723779"
+results = requests.get(forumURL)
 
 # Convert the forum page into a BeautifulSoup object
-soup = BeautifulSoup(forumContent, 'html.parser')
+soup = BeautifulSoup(results.content, 'html.parser')
 
-print(soup.prettify())
+posts = soup.findAll('ul', class_= "lia-message-body-content")
 
-
-
+print(len(posts))
