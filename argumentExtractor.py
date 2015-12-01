295670f41202a6b07cce7e270e0a1a568fe80eb9
Robie Noor <robienoor@hotmail.co.uk>
2015-12-01 20:29:10 +0000

    Added inverter words function to argument Extractor. It is being used in the positiveWordScore function

--- argumentExtractor.py
+++ argumentExtractor.py
@@ -6,93 +6,130 @@ import logging
 import displayArguments
 import messageCleaner
 
-def checkForSymptomInClause(sentence, listOfSymptoms):
+class argumentExtractor(object):
+
+
+    def __init__(self, listOfSymptoms, listOfDiseases, listOfInverters, listOfPosWords, listOfNegWords):
+        self.listOfSymptoms = listOfSymptoms
+        self.listOfDiseases = listOfDiseases
+        self.listIfInverters = listOfInverters
+        self.listOfPosWords = listOfPosWords
+        self.listOfNegWords = listOfNegWords
+
+    def checkForSymptomInClause(self, sentence):
     
-    for symptom in listOfSymptoms:
-        if symptom.lower() in sentence.lower():
-            return (True, symptom)
+        for symptom in self.listOfSymptoms:
+            if symptom.lower() in sentence.lower():
+                return (True, symptom)
         
-    return (False, '')
+        return (False, '')
 
-def checkForDiseaseInClause(sentence, listOfDiseases):
+    def checkForDiseaseInClause(self, sentence):
 
-    for disease in listOfDiseases:
-        if disease.lower() in sentence.lower():
-            return (True, disease)
+        for disease in self.listOfDiseases:
+            if disease.lower() in sentence.lower():
+                return (True, disease)
 
-    return (False,'')
+        return (False,'')
 
-def checkForEmotion(post):
-    ratingCategory = ''
-    volumeofDisSymptms = ''
-    emotion = ''
+    def checkForEmotion(self, post):
+        ratingCategory = ''
+        volumeofDisSymptms = ''
+        emotion = ''
 
-    # Get the rating level
-    rating = float(post.getRating())
-    if rating <= 3:
-        ratingCategory = 'low'
-    elif rating >= 7:
-        ratingCategory = 'high'
-    else:
-        ratingCategory = 'med'
+        # Get the rating level
+        rating = float(post.getRating())
+        if rating <= 3:
+            ratingCategory = 'low'
+        elif rating >= 7:
+            ratingCategory = 'high'
+        else:
+            ratingCategory = 'med'
 
-    # Gauge the volume of symptoms and symptoms
-    noOfSymptoms = len(post.getSymptoms())
-    noOfDiseases = len(post.getDisease())
+        # Gauge the volume of symptoms and symptoms
+        noOfSymptoms = len(post.getSymptoms())
+        noOfDiseases = len(post.getDisease())
   
-    if (noOfDiseases + noOfSymptoms) < 1:
-        volumeofDisSymptms = 'low'
-    elif (noOfDiseases + noOfSymptoms) == 1:
-        volumeofDisSymptms = 'med'
-    else:
-        volumeofDisSymptms = 'high'
+        if (noOfDiseases + noOfSymptoms) < 1:
+            volumeofDisSymptms = 'low'
+        elif (noOfDiseases + noOfSymptoms) == 1:
+            volumeofDisSymptms = 'med'
+        else:
+            volumeofDisSymptms = 'high'
 
-    # Primitive rules for getting the emotion
-    if (volumeofDisSymptms is 'high' and ratingCategory is 'low'):
-        emotion = 'unoptimisitic'
-    elif (volumeofDisSymptms is 'high' and ratingCategory is 'high'):
-        emotion = 'optimistic'
-    elif (volumeofDisSymptms is 'low' and ratingCategory is 'high'):
-        emotion = 'positive'
-    else:
-        emotion = 'neutral'
+        # Primitive rules for getting the emotion
+        if (volumeofDisSymptms is 'high' and ratingCategory is 'low'):
+            emotion = 'unoptimisitic'
+        elif (volumeofDisSymptms is 'high' and ratingCategory is 'high'):
+            emotion = 'optimistic'
+        elif (volumeofDisSymptms is 'low' and ratingCategory is 'high'):
+            emotion = 'positive'
+        else:
+            emotion = 'neutral'
 
-    return emotion
+        return emotion
 
-def calculateWordScore(sentence):
-    # import neg and pos words
-    # count the number of pos words in the sentence
-    # count the number of neg words in the sentence
-    # do a sum of the two: score = posCount - negCount
-    sentenceTokenised = word_tokenize(sentence)
+    def calculateWordScore(self, sentence):
+        # import neg and pos words
+        # count the number of pos words in the sentence
+        # count the number of neg words in the sentence
+        # do a sum of the two: score = posCount - negCount
+        sentenceTokenised = word_tokenize(sentence)
 
-    posWords = 'Data/positive-words.txt'
-    negWords = 'Data/negative-words.txt'
+        #posWords = 'Data/positive-words.txt'
+        #negWords = 'Data/negative-words.txt'
 
-    with open(posWords) as f:
-        posData = f.readlines()
+        #with open(posWords) as f:
+        #    posData = f.readlines()
   
-    posList = []
-    for member in posData: 
-        member = messageCleaner.removeSpecialCharacter(member)
-        posList.append(member)
+        #posList = []
+        #for member in posData: 
+        #    member = messageCleaner.removeSpecialCharacter(member)
+        #    posList.append(member)
 
-    with open(negWords) as f:
-        negData = f.readlines()
+        #with open(negWords) as f:
+        #    negData = f.readlines()
   
-    negList = []
-    for member in negData: 
-        member = messageCleaner.removeSpecialCharacter(member)
-        negList.append(member)
+        #negList = []
+        #for member in negData: 
+        #    member = messageCleaner.removeSpecialCharacter(member)
+        #    negList.append(member)
     
+        negScore = 0
+        posScore = 0
 
-    sentenceTokenisedSet = set(sentenceTokenised)
-    posListSet = set(posList)
-    negListSet = set(negList)
+        for i, word in enumerate(sentenceTokenised):
+            score = self.__searchForMatchingToken(word, i, sentenceTokenised, self.listOfPosWords)
 
-    posMatches = sentenceTokenisedSet.intersection(posListSet)
-    negMatches = sentenceTokenisedSet.intersection(negListSet)
+            if score < 0:
+                negScore = negScore + score
+            else:
+                posScore = posScore + score
 
-    score = len(posMatches) - len(negMatches)
+        totalScore = posScore + negScore
 
-    return score
+        return totalScore
+
+    def __searchForMatchingToken(self, word, index, sentence, list):
+
+        """This method will search for inverters like 'not', 'no' etc. 
+            It works as follows:
+                1. Search to see if the word exists in the list you are searching in
+                    1. If it is the first word in the sentence (known from its index) assume it as a positive existance and return 1
+                    2. If it is not the first word in the sentence, it may be that there is an inverter before it
+                        1. If there is an inverter assume that it is a negative existance and return -1
+                        2. Id there is not an inveter assum that it is a positive existance and return 1
+                    3. If the word does not exist at all return 0
+        """
+
+        if word in list:
+            if index > 0:
+                if sentence[index - 1] in self.listIfInverters:
+                    return -1
+                else:
+                    return 1
+            else:
+                return 1
+        else:
+            return 0
+
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import postsGatherer
 import forum
 import logging
 import displayArguments
-import argumentExtractor
+from argumentExtractor import argumentExtractor
+import messageCleaner
 
 def main():
     logging.basicConfig(filename='log.log', level = logging.DEBUG, format='%(asctime)s %(message)s')
@@ -15,20 +16,51 @@ def main():
     logging.info('-------------Starting Argument Extractor---------------')
     logging.info('-------------------------------------------------------')
 
+    logging.info('Starting [main]: Building data lists')
     forums = postsGatherer.gatherForums()
     listOfDiseases = medicalInfoGatherer.getListofDiseases()
     listOfSymptoms = medicalInfoGatherer.getListofSymptoms()
 
-    symptomsFound = []
-    diseasesFound = []
+    posWords = 'Data/positive-words.txt'
+    negWords = 'Data/negative-words.txt'
+    invWords = 'Data/inverter-words.txt'
 
+    with open(posWords) as f:
+        posData = f.readlines()
+  
+    posList = []
+    for member in posData: 
+        member = messageCleaner.removeSpecialCharacter(member)
+        posList.append(member)
+
+    with open(negWords) as f:
+        negData = f.readlines()
+  
+    negList = []
+    for member in negData: 
+        member = messageCleaner.removeSpecialCharacter(member)
+        negList.append(member)
+
+    with open(invWords) as f:
+        invData = f.readlines()
+  
+    invList = []
+    for member in invData: 
+        member = messageCleaner.removeSpecialCharacter(member)
+        invList.append(member)
+
+
+    
+    logging.info('Finished [main]: Finished building data lists')
+    
+    argExtractor = argumentExtractor(listOfSymptoms, listOfDiseases, invList, posList, negList)
     # Loop through each of the forum posts and print the arguments onto the page. The arguments for and against each of the posts.
     # Some sort of extremley Naive Bayesian classifier 
 
     for forum in forums:
         posts = forum.getPosts()
 
-        logging.info('Starting [argumentExtractor]: Beginning to search for symptoms and diseases within the posts')
+        logging.info('Starting [main]: Beginning to search for symptoms and diseases within the posts')
 
         for post in posts:
             sentences = nltk.sent_tokenize(post.getReview())
@@ -36,28 +68,28 @@ def main():
             postWordScore = 0
 
             for sentence in sentences:
-                    
-                postWordScore = postWordScore + argumentExtractor.calculateWordScore(sentence)
 
-                foundSymptom , symptom = argumentExtractor.checkForSymptomInClause(sentence, listOfSymptoms)
+                postWordScore = postWordScore + argExtractor.calculateWordScore(sentence)
+
+                foundSymptom , symptom = argExtractor.checkForSymptomInClause(sentence)
                 # Make sure that we do not append a symptom twice.It may be the case that the post mentions a symptom twice in two or more sentences
                 if(foundSymptom and symptom not in post.getSymptoms()):
                     post.setSymptoms(symptom)
 
-                foundDisease , disease = argumentExtractor.checkForDiseaseInClause(sentence, listOfDiseases)
+                foundDisease , disease = argExtractor.checkForDiseaseInClause(sentence)
                 if(foundDisease and disease not in post.getDisease()):
                     post.setDisease(disease)
 
             post.setPositiveWordScore(str(postWordScore))
-            emotion = argumentExtractor.checkForEmotion(post)
+            emotion = argExtractor.checkForEmotion(post)
             post.setEmotion(emotion)
 
-        logging.info('Completed [argumentExtractor]: Finished searching for symptoms and diseases within the posts')
+        logging.info('Completed [main]: Finished searching for symptoms and diseases within the posts')
 
     # Now run through and print off the list of symptoms and diseases found relative to the drug
     # In this simple script we have taken the reviews on tamoxifen 
     displayArguments.constructXML(forums)
-    logging.info('Finished [argumentExtractor]: Finished Process Succesfully')
+    logging.info('Finished [main]: Finished Process Succesfully')
 
 
 if __name__ == "__main__": main()
